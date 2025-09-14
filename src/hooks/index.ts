88b1a46b7e6c8732fce659f3c0fff6b33966fc44
gram12321_laptop <34036134+gram12321@users.noreply.github.com>
2025-09-14 23:36:25 +0200

    0.0032a cleaning

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,14 +1,7 @@
-// Barrel export for hooks to reduce import duplication
-
-// Game state hooks
 export { useGameState, useGameStateWithData } from './useGameState';
 export { useGameUpdates } from './useGameUpdates';
 export { useGameInit } from './useGameInit';
 export { usePrestigeUpdates } from './usePrestigeUpdates';
-
-// Utility hooks
 export { useLoadingState } from './useLoadingState';
 export { useTableSortWithAccessors } from './useTableSort';
-
-// Type exports
 export type { SortableColumn } from './useTableSort';
--- src/lib/utils/index.ts
+++ src/lib/utils/index.ts
@@ -1,56 +1,6 @@
-// Barrel export for common utilities to reduce import duplication
-
-// Formatting utilities
-export { 
-  formatNumber, 
-  formatCurrency, 
-  formatDate, 
-  formatGameDate, 
-  formatGameDateFromObject,
-  formatPercent,
-  formatTime,
-  formatCompact,
-  getColorClass,
-  calculateCompanyWeeks
-} from './utils';
-
-// Calculation utilities
-export { 
-  calculateSteppedBalance,
-  calculateAsymmetricalMultiplier,
-  getRandomAcres,
-  farmlandAgePrestigeModifier,
-  calculateBaseWinePrice,
-  calculateSymmetricalMultiplier,
-  calculateOrderAmount
-} from './calculator';
-
-// Company utilities
-export { 
-  getCurrentCompanyId,
-  getCompanyQuery,
-  getCompanyDeleteQuery,
-  getCompanyUpdateQuery,
-  insertCompanyRecord,
-  upsertCompanyRecord,
-  getAllCompanyRecords,
-  getCompanyRecord
-} from './companyUtils';
-
-// Flag utilities
-export { 
-  getFlagIcon,
-  getCountryFlag
-} from './flags';
-
-// Wine utilities
-export { 
-  getAvailableBottledWines,
-  loadFormattedRelationshipBreakdown
-} from './UIWineFilters';
-
-
-// Emoji utilities
-export { 
-  NAVIGATION_EMOJIS
-} from './emojis';
+export { formatNumber, formatCurrency, formatDate, formatGameDate, formatGameDateFromObject, formatPercent, formatTime, formatCompact, getColorClass, calculateCompanyWeeks } from './utils';
+export { calculateSteppedBalance, calculateAsymmetricalMultiplier, getRandomAcres, farmlandAgePrestigeModifier, calculateBaseWinePrice, calculateSymmetricalMultiplier, calculateOrderAmount } from './calculator';
+export { getCurrentCompanyId, getCompanyQuery, getCompanyDeleteQuery, getCompanyUpdateQuery, insertCompanyRecord, upsertCompanyRecord, getAllCompanyRecords, getCompanyRecord } from './companyUtils';
+export { getFlagIcon, getCountryFlag } from './flags';
+export { getAvailableBottledWines, loadFormattedRelationshipBreakdown } from './UIWineFilters';
+export { NAVIGATION_EMOJIS } from './emojis';
